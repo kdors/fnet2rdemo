711cb5fb7f3453c3ef2d01ee3a448370e6db8729
Margaux Armfield <margauxarmfield@Margauxs-MacBook-Air.local>
2019-11-18 01:11:30 -0600

    hastissue()

---
+++ R/hasTissue.R
@@ -0,0 +1,22 @@
+#' Summarize a set of records downloaded from FishNet2
+#'
+#' Filters data returned by a FishNet2 search for records that include tissue information.
+#'
+#' @export
+#' @param input A dataframe in FishNet2 standard format (by using read.csv())
+#' @param verbose Print progress and information messages. Default: TRUE
+#' @return Filtered dataset with records that do not have a blank tissue value
+#' 
+#' # summarize occurrence records
+#' 
+hasTissues <- function(input, verbose = TRUE){
+  output <- input$Tissues[!is.na(input$Tissues)]
+  total <- length(output)
+  if(verbose == TRUE){
+    print("Records with tissues: ")
+    print(total)
+  }
+  return(output)
+}
+  
+
