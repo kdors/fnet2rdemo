b24031815c5b63e40a8a34d991a6248a2248be91
Kennedy Dorsey <kadorsey97@gmail.com>
2020-04-10 10:17:43 -0500

    Update top_n_plots to take in any column name

--- R/top_n_plots.R
+++ R/top_n_plots.R
@@ -13,38 +13,24 @@
 #' @param df A dataframe in FishNet2 standard format (by using read.csv())
 #' @param n The number of the labels with the highest frequencies to be included
 #'   in the graph
-#' @param col The column name that the graph outputs
+#' @param colName The column name that the graph outputs
 #' @return A bar graph
 #'
 #' @examples
-#' top_n_plots(countries_search)
+#' top_n_plots(countries_search,10,"ScientificName")
 
-top_n_plots <- function(df,n,col){
-  sntable<-sort(table(df$ScientificName),decreasing=T)
+top_n_plots <- function(df,n,colName){
+  #check if column exists
+  if (colName %in% colnames(df) == FALSE){
+    stop("Column does not exist.")
+  }
+  sntable<-sort(table(df[[colName]]),decreasing=T)
   sntable <-head(sntable,n)
 
-  df <- subset(df, ScientificName %in% names(sntable))
-  df <- within(df, ScientificName <- factor(ScientificName,levels=names(sort(table(ScientificName),decreasing=TRUE))))
-
-  ggplot(data = df) + geom_bar(mapping = aes(x = ScientificName, fill = ScientificName)) + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+  df <- df[df[,colName] %in% names(sntable),]
+  df <- within(df, colName <- factor(colName,levels=names(sort(table(colName),decreasing=TRUE))))
+  ggplot(data = df) + geom_bar(mapping = aes_string(x = colName, fill = colName)) + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
 }
 
-
-
-
-
-
-
-
-sntable<-sort(table(ictaluridae$ScientificName),decreasing=T)
-sntable <-head(sntable,10)
-
-ict2 <- subset(ictaluridae, ScientificName %in% names(sntable))
-ict2 <- within(ict2, ScientificName <- factor(ScientificName,
-                                                levels=names(sort(table(ScientificName),
-                                                                  decreasing=TRUE))))
-
-ggplot(data = ict2) + geom_bar(mapping = aes(x = ScientificName, fill = ScientificName))
-                                + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
