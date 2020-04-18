bd9229e861e5711d507e9f3f6a5ffe79231e7530
Margaux Armfield <margauxarmfield@Margauxs-MacBook-Air.local>
2020-04-18 16:29:08 -0400

    fix has_tissues

--- R/has_tissue.R
+++ R/has_tissue.R
@@ -18,9 +18,8 @@ hasTissues <- function(input, verbose = TRUE){
 #' has_tissue(louisiana,TRUE)
 #'
 has_tissue <- function(input, verbose = TRUE){
-  output <- input$Tissues[!is.na(input$Tissues)]
-  total <- length(output)
->>>>>>> e3d5e204efec314f3da1163056083bec6761d5e9
+  output <- subset(input, is.na(input$Tissues) == FALSE)
+  total <- length(output$Tissues)
   if(verbose == TRUE){
     print("Records with tissues: ")
     print(total)
