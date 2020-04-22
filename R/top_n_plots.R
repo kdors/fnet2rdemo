787fbdbe7a50b90ded6f191555402d6659607880
Kennedy Dorsey <kadorsey97@gmail.com>
2020-04-22 15:28:12 -0500

    Add ability to choose graph color to top_n_plots

--- R/top_n_plots.R
+++ R/top_n_plots.R
@@ -11,16 +11,18 @@
 #'
 #' @export
 #' @importFrom utils head
+#' @importFrom grDevices colours
 #' @param df A dataframe in FishNet2 standard format (by using read.csv())
 #' @param n The number of the labels with the highest frequencies to be included
 #'   in the graph
 #' @param colName The column name that the graph outputs
+#' @param color Color of the bars, by default is a different color for each bar
 #' @return A bar graph
 #'
 #' @examples
 #' top_n_plots(ictaluridae,10,"ScientificName")
 
-top_n_plots <- function(df,n,colName){
+top_n_plots <- function(df,n,colName, color = 'default colors'){
   #check if column exists
   if (colName %in% colnames(df) == FALSE){
     stop("Column does not exist.")
@@ -30,8 +32,15 @@ top_n_plots <- function(df,n,colName){
 
   df <- df[df[,colName] %in% names(sntable),]
   df <- within(df, colName <- factor(colName,levels=names(sort(table(colName),decreasing=TRUE))))
-  ggplot2::ggplot(data = df) + ggplot2::geom_bar(mapping = ggplot2::aes_string(x = colName, fill = colName)) + ggplot2::theme(axis.text.x= ggplot2::element_text(angle=90,hjust=1,vjust=0.5))
-
+  if (color == "default colors"){
+    ggplot2::ggplot(data = df) + ggplot2::geom_bar(mapping = ggplot2::aes_string(x = colName, fill = colName)) + ggplot2::theme(axis.text.x= ggplot2::element_text(angle=90,hjust=1,vjust=0.5))
+  }
+  else {
+    if(!color %in% colours()){
+      stop("Error: Color not found")
+    }
+    ggplot2::ggplot(data = df) + ggplot2::geom_bar(mapping = ggplot2::aes_string(x = colName),color=color, fill=color) + ggplot2::theme(axis.text.x= ggplot2::element_text(angle=90,hjust=1,vjust=0.5))
+  }
 }
 
 
